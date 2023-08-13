2281b433ece01a7ae3917a9ecfcc05aecb3e0f78
runingcoder <achyutkayastha5@gmail.com>
2023-08-13 16:07:33 +0545

    DIP description done with bad examples.

--- public/DIP.js
+++ public/DIP.js
@@ -1,1 +1,20 @@
 "use strict";
+// For bad implementation, we can have this, 
+// export class MainFeeCalculator {   
+//     constructor() {} 
+//      createCalculator(background: string, appearanceFrequency: number): RegistrationFeeCalculator {
+//       switch (background) {
+//         case 'Private':
+//           return new PrivateCollegeRegistrationFeeCalculator(appearanceFrequency);
+//         case 'Governmental':
+//           return new GovCollegeRegistrationFeeCalculator(appearanceFrequency);
+//         case 'International':
+//           return new InternationalCollegeRegistrationFeeCalculator(appearanceFrequency);
+//         // Add more cases for additional fee calculator types
+//         default:
+//           throw new Error(`Unsupported background: ${background}`);
+//       }
+//     }
+//   }
+// Here, the higher level module is dependent on the lower level module directly.
+// So, for a good implementation, look in the OCP.ts file.
